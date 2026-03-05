0074f3af5b70684e8ef4ab795c570ac6e17b2e86
basavarajck <basavarajck2005@gmail.com>
2026-03-05 16:54:23 +0530

    email added

--- server/src/config/adminEmails.js
+++ server/src/config/adminEmails.js
@@ -1,4 +1,11 @@
 module.exports = [
   "gcn3888@gmail.com",
-  "varun030406@gmail.com"
+  "varun030406@gmail.com",
+  "Ikigai114@gmail.com",
+  "vaibhavns808@gmail.com",
+  "aniwars300@gmail.com",
+  "prateek.n.vasista@gmail.com",
+  "pranavgn2005@gmail.com",
+  "thrupthishetty2005@gmail.com",
+  "vinayakkamath17@gmail.com"
 ];
